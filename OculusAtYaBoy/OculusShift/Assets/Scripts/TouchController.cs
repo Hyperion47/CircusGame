7f219101c76a3bf8105bdaa886d5bb2a739cff26
Hyperion47 <37087822+Hyperion47@users.noreply.github.com>
2018-09-15 15:56:44 -0400

    added the unity project

---
+++ OculusAtYaBoy/OculusShift/Assets/Scripts/TouchController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+    public OVRInput.Controller Controller;
+
+    // Update is called once per frame
+    void Update () {
+        OVRInput.Update();
+        transform.localPosition = (OVRInput.GetLocalControllerPosition(Controller) * 10);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(Controller);
+    }
+}
